220b973afb992bd1d44811723357ca2a4672e1ad
lihua <yehong0000@163.com>
2018-06-27 13:03:36 +0800

    简版测试

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # raft-example
-raft测试示例代码
+raft 学习测试代码
+- 启动
+```golang
+go run main.go
+```
+- 简单的 web 接口
+```shell
+#获取一个key的值
+curl 127.0.0.1:6061?get=k
+#设置一个key的值
+curl 127.0.0.1:6061?set=k:v
+#删除一个key
+curl 127.0.0.1:6061?delete=k
+```
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	node.Start(true)
 	store := service.NewStore(node.GetRaftInstince())
 	handel := service.NewWeb(store)
-	if err := http.ListenAndServe(":8080", handel); err != nil {
+	if err := http.ListenAndServe(":6061", handel); err != nil {
 		log.Fatal("HTTP服务启动失败:", err)
 	}
 }
